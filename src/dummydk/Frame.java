53cdd183cc5c7128f495ba796861daef1a121f4b
Philipp Giesen <philipp.giesen@uni-duesseldorf.de>
2013-06-07 21:26:22 +0200

    Method stubs for Dungeon Keeper dummy program

---
+++ src/dummydk/Frame.java
@@ -0,0 +1,17 @@
+package dummydk;
+
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+
+public abstract class Frame extends JFrame implements KeyListener {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/example/SimpleJFrame.java
+++ src/example/SimpleJFrame.java
@@ -1,3 +1,4 @@
+package example;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
